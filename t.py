9cabb495e831dd7a3d9b2da08faa7116161947c8
Aran Carmon <aran.carmon@gmail.com>
2021-05-16 18:37:12 +0300

    .

--- t.py
+++ t.py
@@ -2,10 +2,10 @@ import json
 
 j = json.load(open("../bookmarks-2021-05-16.json"))
 
-print("<html>")
-print("<body>")
+#print("<html>")
+#print("<body>")
 for _ in j['children'][1]['children'][3]['children']: 
     print(f"<a href=\"{_['uri']}\">{_['title']}</a> <br>")                                                                                                     
-print("</body>")
-print("</html>")
+#print("</body>")
+#print("</html>")
 print()
