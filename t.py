39b67af1f80de2c38c9e566297432ed38cdc6d53
Aran Carmon <aran.carmon@gmail.com>
2021-05-16 18:33:07 +0300

    .

--- t.py
+++ t.py
@@ -3,8 +3,10 @@ import json
 j = json.load(open("../bookmarks-2021-05-16.json"))
 
 print("<html>")
+print("<body>")
 for _ in j['children'][1]['children'][3]['children']: 
     print(f" <\"{_['uri']}\">{_['title']}</a> ")                                                                                                     
+print("</body>")
 print("</html>")
 
 
